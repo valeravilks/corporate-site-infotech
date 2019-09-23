6a17e7cbad054a4db6d3139c089c3380c0aa3519
Valera Vilks <valeravilks@gmail.com>
2019-09-23 15:01:30 +0300

    Add main slide

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".js-main-slider").owlCarousel({
+        items: 1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000
+    });
+});
