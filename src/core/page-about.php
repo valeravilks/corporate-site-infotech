1fc9e67cac501ea7f03876e639b4fbdc79d0726e
Valera Vilks <valeravilks@gmail.com>
2019-10-11 16:48:48 +0300

    Scolkovo base

---
+++ src/core/page-about.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Template Name: Направления деятельности
+ */
+
+get_header(); ?>
+<section class="about">
+    <div class="wrapper">
+        <?php get_template_part('templates/mini-menu'); ?>
+        <div class="modern">
+            <div class="col-1">
+                <div class="title">
+                    Современный подход к автоматизации и решению повседневных задач бизнеса
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img">
+                </div>
+                <div class="text-1">
+                    Ознакомьтесь с основными направлениями деятельности компании
+                </div>
+                <a href="#" class="link">
+                    Скачать буклет
+                </a>
+            </div>
+        </div>
+        <div class="head-2">
+            Основные направления деятельности компании
+        </div>
+        <div class="to">
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+            <div class="col">
+                <div class="img"></div>
+                <div class="title">
+                    Внедрение программных продуктов
+                </div>
+                <div class="text">
+                    Внедряем программные продукты собственной разработки в сферы энергетики, судостроения, учета имущества, здравоохранения, обеспечения безопасности и охраны
+                </div>
+            </div>
+        </div>
+        <div class="n-block">
+            <div class="title">
+                О нас пишут
+            </div>
+            <div class="abouts">
+                <div class="col-1">
+                    <div class="logo"></div>
+                    <div class="date">
+                        12.08.18
+                    </div>
+                    <div class="text">
+                        Infotech Group сфокусируется на прогнозных системах
+                    </div>
+                    <a href="#" class="link">Читать далее</a>
+                </div>
+                <div class="col-2">
+                    <div class="logo"></div>
+                    <div class="date">
+                        12.08.18
+                    </div>
+                    <div class="text">
+                        Infotech Group сфокусируется на прогнозных системахsdfsfsdf sd d sdf sdf
+                    </div>
+                    <a href="#" class="link">Читать далее</a>
+                </div>
+                <div class="col-3">
+                    <div class="logo"></div>
+                    <div class="date">
+                        12.08.18
+                    </div>
+                    <div class="text">
+                        Infotech Group сфокусируется на прогнозных системах
+                    </div>
+                    <a href="#" class="link">Читать далее</a>
+                </div>
+            </div>
+            <a href="#" class="to-news">Перейти ко всем новостям</a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
+
---
+++ src/core/page-scolkovo.php
@@ -0,0 +1,244 @@
+<?php
+
+/**
+ * Template Name: Проект в Сколково
+ */
+
+get_header(); ?>
+<section class="scolkovo">
+    <div class="wrapper">
+        <?php get_template_part('templates/mini-menu'); ?>
+        <div class="title">
+            Проект в Сколково
+        </div>
+        <div class="img">
+            <img class="in-img" src="" alt="">
+        </div>
+        <div class="children">
+            <div class="col-1">
+                <div class="logo"></div>
+                <div class="text">
+                    Дочерняя компания Infotech Group
+                </div>
+                <a href="#" class="link">
+                    holocron.tech
+                </a>
+            </div>
+            <div class="col-2">
+                <div class="text-2">
+                    Интеллектуальный анализ и проактивное управление техническим состоянием объектов и техники
+                </div>
+            </div>
+        </div>
+        <div class="dop">
+            <div class="col">
+                <div class="img-d">
+                </div>
+                <div class="text">
+                    Holocron разработала автоматизированную систему для контроля, диагностики и прогнозирования состояния технологических объектов и техники
+                </div>
+            </div>
+            <div class="col">
+                <div class="img-d">
+                </div>
+                <div class="text">
+                    Holocron разработала автоматизированную систему для контроля, диагностики и прогнозирования состояния технологических объектов и техники
+                </div>
+            </div>
+            <div class="col">
+                <div class="img-d">
+                </div>
+                <div class="text">
+                    Holocron разработала автоматизированную систему для контроля, диагностики и прогнозирования состояния технологических объектов и техники
+                </div>
+            </div>
+        </div>
+        <div class="analog">
+            <div class="block">
+                <div class="col-1">
+                    <div class="text-1">
+                        Инновационная составляющая
+                    </div>
+                    <div class="text-2">
+                        Уникальное сочетание технологий
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="text-3">
+                        Система, разработанная Holocron, превосходит существующие отраслевые решения, дополняя их возможности технологиями Fog computing и Интернета вещей: Event Stream Processing + IoT Analytics
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="technolog">
+        <div class="wrapper">
+            <div class="title-t">
+                Стек технологий
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon"></div>
+                    <div class="text">
+                        Raspberry Pi
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="benefits">
+            <div class="head">
+                Преимущества
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="icon">
+
+                    </div>
+                    <div class="text-1">
+                        Адаптивность
+                    </div>
+                    <div class="text-2">
+                        Созданная для электросетевых компаний система построена на базе универсальной технологической платформы, что позволяет легко адаптировать решение для других отраслей. Система работает с данными из различных источников, обеспечивая достоверную и полную картину происходящего с объектом мониторинга.
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+
+                    </div>
+                    <div class="text-1">
+                        Адаптивность
+                    </div>
+                    <div class="text-2">
+                        Созданная для электросетевых компаний система построена на базе универсальной технологической платформы, что позволяет легко адаптировать решение для других отраслей. Система работает с данными из различных источников, обеспечивая достоверную и полную картину происходящего с объектом мониторинга.
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+
+                    </div>
+                    <div class="text-1">
+                        Адаптивность
+                    </div>
+                    <div class="text-2">
+                        Созданная для электросетевых компаний система построена на базе универсальной технологической платформы, что позволяет легко адаптировать решение для других отраслей. Система работает с данными из различных источников, обеспечивая достоверную и полную картину происходящего с объектом мониторинга.
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon">
+
+                    </div>
+                    <div class="text-1">
+                        Адаптивность
+                    </div>
+                    <div class="text-2">
+                        Созданная для электросетевых компаний система построена на базе универсальной технологической платформы, что позволяет легко адаптировать решение для других отраслей. Система работает с данными из различных источников, обеспечивая достоверную и полную картину происходящего с объектом мониторинга.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="decision">
+            <div class="col-1">
+                <div class="head">
+                    Отраслевые решения на базе технологии
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="img"></div>
+                <div class="text-1">
+                    Решение для энергетических компаний
+                </div>
+                <div class="text-2">
+                    INFOTECH RESULTAT
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="img"></div>
+                <div class="text-1">
+                    Решение для энергетических компаний
+                </div>
+                <div class="text-2">
+                    INFOTECH RESULTAT
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
+
